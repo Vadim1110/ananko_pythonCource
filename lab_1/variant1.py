c4d37da0a8d21b8b2c5803a6173e7ba0ec5fb178
Vadim1110 <117761831+Vadim1110@users.noreply.github.com>
2025-03-12 21:40:01 +0200

    Add files via upload

---
+++ lab_1/variant1.py
@@ -0,0 +1,21 @@
+from PIL.ImImagePlugin import number
+from sympy import numer
+
+x = 0
+lst = [
+    [1, 2, 5, 6, ],
+    [3, 5, 0, 6, ],
+    [8, 1, 7, 10]
+]
+
+for row in lst:
+    if 0 not in row:
+        x += 1
+
+max_value = float('-inf')
+for row in lst:
+    for number in row:
+        if number > max_value:
+            max_value = number
+
+print('lines without 0: ' + str(x), '| max number: ' + str(max_value))
